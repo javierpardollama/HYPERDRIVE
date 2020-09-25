9c89acde20c55a213bbb60c52880b560b15e728a
Javier Pardo <javierpardollama@outlook.com>
2020-09-25 21:25:34 +0200

    TESTS

---
+++ Hyperdrive.Portal/Hyperdrive.Tier.Services.Tests/Classes/TestBaseService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+using AutoMapper;
+
+using Hyperdrive.Tier.Contexts.Classes;
+using Hyperdrive.Tier.Entities.Classes;
+using Hyperdrive.Tier.Mappings.Classes;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hyperdrive.Tier.Services.Tests.Classes
+{
+    /// <summary>
+    /// Represents a <see cref="TestBaseService"/> class.
+    /// </summary>
+    public abstract class TestBaseService
+    {
+        /// <summary>
+        /// Instance of <see cref="IMapper"/>
+        /// </summary>
+        public IMapper Mapper;
+
+        /// <summary>
+        /// Instance of <see cref="IConfiguration"/>
+        /// </summary>
+        public IConfiguration Configuration;
+
+        /// <summary>
+        /// Instance of <see cref="Dictionary{string, string}"/>
+        /// </summary>
+        private Dictionary<string, string> JwtSettings;
+
+        /// <summary>
+        /// Instance of <see cref="DbContextOptions{ApplicationContext}"/>
+        /// </summary>
+        private DbContextOptions<ApplicationContext> Options;
+
+        /// <summary>
+        /// Instance of <see cref="ApplicationContext"/>
+        /// </summary>
+        public ApplicationContext Context;
+
+        /// <summary>
+        /// Instance of <see cref="UserManager{ApplicationUser}"/>
+        /// </summary>
+        public UserManager<ApplicationUser> UserManager;
+
+        /// <summary>
+        /// Instance of <see cref="SignInManager{ApplicationUser}"/>
+        /// </summary>
+        public SignInManager<ApplicationUser> SignInManager;
+
+        /// <summary>
+        /// Instance of <see cref="ServiceCollection"/>
+        /// </summary>
+        private ServiceCollection Services;
+
+        /// <summary>
+        /// Instance of <see cref="ServiceProvider"/>
+        /// </summary>
+        private ServiceProvider ServiceProvider;
+
+        /// <summary>
+        /// Sets Up Services
+        /// </summary>
+        public void SetUpServices()
+        {
+            Services = new ServiceCollection();
+
+            Services
+                .AddSingleton(Configuration)
+                .AddDbContext<ApplicationContext>(o => o.UseSqlite("Data Source=Hyperdrive.db"))
+                .AddIdentity<ApplicationUser, ApplicationRole>(options =>
+                {
+                    options.Lockout = new LockoutOptions()
+                    {
+                        AllowedForNewUsers = true,
+                        DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5),
+                        MaxFailedAccessAttempts = 5
+                    };
+                })
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+            Services.AddLogging();
+
+            ServiceProvider = Services.BuildServiceProvider();
+
+            Context = new ApplicationContext(Options);
+            UserManager = ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            SignInManager = ServiceProvider.GetRequiredService<SignInManager<ApplicationUser>>();
+        }
+
+        /// <summary>
+        /// Sets Up Mapper
+        /// </summary>
+        public void SetUpMapper()
+        {
+            MapperConfiguration @config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ModelingProfile());
+            });
+
+            Mapper = @config.CreateMapper();
+        }
+
+        /// <summary>
+        /// Sets Up Jwt Settings
+        /// </summary>
+        public void SetUpJwtSettings()
+        {
+            JwtSettings = new Dictionary<string, string>
+            {
+                { "Jwt:JwtKey", "SOME_RANDOM_KEY_DO_NOT_SHARE"},
+                { "Jwt:JwtIssuer", "http://localhost:15208"},
+                { "Jwt:JwtAudience", " http://localhost:4200"},
+                { "Jwt:JwtExpireDays", "30"},
+            };
+        }
+
+        /// <summary>
+        /// Sets Up Configuration
+        /// </summary>
+        public void SetUpConfiguration()
+        {
+            Configuration = new ConfigurationBuilder().AddInMemoryCollection(JwtSettings).Build();
+        }
+
+        /// <summary>
+        /// Sets Up Options
+        /// </summary>
+        public void SetUpOptions()
+        {
+            Options = new DbContextOptionsBuilder<ApplicationContext>()
+           .UseInMemoryDatabase(databaseName: "Data Source=Hyperdrive.db")
+           .Options;
+        }
+    }
+}
---
+++ Hyperdrive.Portal/Hyperdrive.Tier.Services.Tests/Classes/TestSecurityService.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Hyperdrive.Tier.Entities.Classes;
+using Hyperdrive.Tier.Services.Classes;
+using Hyperdrive.Tier.ViewModels.Classes.Security;
+using Hyperdrive.Tier.ViewModels.Classes.Views;
+
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+namespace Hyperdrive.Tier.Services.Tests.Classes
+{
+    /// <summary>
+    /// Represents a <see cref="TestSecurityService"/> class. Inherits <see cref="TestBaseService"/>
+    /// </summary>
+    [TestFixture]
+    public class TestSecurityService : TestBaseService
+    {
+        /// <summary>
+        /// Instance of <see cref="ILogger{SecurityService}"/>
+        /// </summary>
+        private ILogger<SecurityService> Logger;
+
+        /// <summary>
+        /// Instance of <see cref="TokenService"/>
+        /// </summary>>
+        private TokenService TokenService;
+
+        /// <summary>
+        /// Instance of <see cref="SecurityService"/>
+        /// </summary>
+        private SecurityService Service;
+
+
+        /// <summary>
+        /// Initializes a new Instance of <see cref="TestSecurityService"/>
+        /// </summary>
+        public TestSecurityService()
+        {
+
+        }
+
+        /// <summary>
+        /// Sets Up
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            SetUpJwtSettings();
+
+            SetUpConfiguration();
+
+            SetUpOptions();
+
+            SetUpServices();
+
+            SetUpMapper();
+
+            SetUpLogger();
+
+            SetUpContext();
+
+            TokenService = new TokenService(Configuration);
+
+            Service = new SecurityService(Mapper, Logger, Configuration, UserManager, TokenService);
+        }
+
+        /// <summary>
+        /// Tears Down
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            Context.ApplicationUser.RemoveRange(Context.ApplicationUser.ToList());
+
+            Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Sets Up Logger
+        /// </summary>
+        private void SetUpLogger()
+        {
+            ILoggerFactory @loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddConsole();
+            });
+
+            Logger = @loggerFactory.CreateLogger<SecurityService>();
+        }
+
+        /// <summary>
+        /// Sets Up Context
+        /// </summary>
+        private void SetUpContext()
+        {
+            Context.ApplicationUser.Add(new ApplicationUser { PasswordHash = "dcb97c304778b75e4309bdd51d61c906dc184cd37df1256fdafd3e54cf6218bb", UserName = "firstuser@email.com", Email = "firstuser@email.com", LastModified = DateTime.Now, Deleted = false, ConcurrencyStamp = new Guid().ToString(), SecurityStamp = new Guid().ToString(), ApplicationUserRoles = new List<ApplicationUserRole>() });
+            Context.ApplicationUser.Add(new ApplicationUser { PasswordHash = "dcb97c304778b75e4309bdd51d61c906dc184cd37df1256fdafd3e54cf6218bb", UserName = "seconduser@email.com", Email = "seconduser@email.com", LastModified = DateTime.Now, Deleted = false, ConcurrencyStamp = new Guid().ToString(), SecurityStamp = new Guid().ToString(), ApplicationUserRoles = new List<ApplicationUserRole>() });
+            Context.ApplicationUser.Add(new ApplicationUser { PasswordHash = "dcb97c304778b75e4309bdd51d61c906dc184cd37df1256fdafd3e54cf6218bb", UserName = "thirstuser@email.com", Email = "thirdtuser@email.com", LastModified = DateTime.Now, Deleted = false, ConcurrencyStamp = new Guid().ToString(), SecurityStamp = new Guid().ToString(), ApplicationUserRoles = new List<ApplicationUserRole>() });
+
+            Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Finds Application User By Email
+        /// </summary>
+        /// <returns>Instance of <see cref="Task"/></returns>
+        [Test]
+        public async Task FindApplicationUserByEmail()
+        {
+            await Service.FindApplicationUserByEmail(Context.ApplicationUser.FirstOrDefault().Email);
+
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Resets Password
+        /// </summary>
+        /// <returns>Instance of <see cref="Task"/></returns>
+        [Test]
+        public async Task ResetPassword()
+        {
+            SecurityPasswordReset viewModel = new SecurityPasswordReset()
+            {
+                Email = Context.ApplicationUser.FirstOrDefault(x => x.Email == "firstuser@email.com").Email,
+                NewPassword = "P@55w0rd"
+            };
+
+            await Service.ResetPassword(viewModel);
+
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Changes Password
+        /// </summary>
+        [Test]
+        public void ChangePassword()
+        {
+            SecurityPasswordChange viewModel = new SecurityPasswordChange()
+            {
+                CurrentPassword = "P@55w0rd",
+                NewPassword = "P@55w0rd",
+                ApplicationUser = new ViewApplicationUser
+                {
+                    Id = Context.ApplicationUser.FirstOrDefault(x => x.Email == "seconduser@email.com").Id,
+                    Email = Context.ApplicationUser.FirstOrDefault(x => x.Email == "seconduser@email.com").Email
+                }
+            };
+
+            Exception exception = Assert.ThrowsAsync<Exception>(async () => await Service.ChangePassword(viewModel));
+
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Changes Email
+        /// </summary>
+        /// <returns>Instance of <see cref="Task"/></returns>
+        [Test]
+        public async Task ChangeEmail()
+        {
+            SecurityEmailChange viewModel = new SecurityEmailChange()
+            {
+                NewEmail = "newthirduser@email.com",
+                ApplicationUser = new ViewApplicationUser
+                {
+                    Id = Context.ApplicationUser.FirstOrDefault(x => x.Email == "thirdtuser@email.com").Id,
+                    Email = Context.ApplicationUser.FirstOrDefault(x => x.Email == "thirdtuser@email.com").Email
+                }
+            };
+
+            await Service.ChangeEmail(viewModel);
+
+            Assert.Pass();
+        }
+    }
+}
