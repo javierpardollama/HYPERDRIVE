333bf95954e28e9ff56e4a317520048e79e1d3d0
Javier Pardo <javierpardollama@outlook.com>
2021-01-11 11:43:30 +0100

    ERROR DE MAPEO DE LOGS

--- Hyperdrive.Portal/Hyperdrive.Tier.Mappings/Classes/LoggingProfile.cs
+++ Hyperdrive.Portal/Hyperdrive.Tier.Mappings/Classes/LoggingProfile.cs
@@ -20,10 +20,12 @@ namespace Hyperdrive.Tier.Mappings.Classes
     { ApplicationEvents.InsertItem, LogLevel.Information },
     { ApplicationEvents.UpdateItem, LogLevel.Information },
     { ApplicationEvents.DeleteItem, LogLevel.Information },
-    { ApplicationEvents.UserAuthenticated, LogLevel.Information },
     { ApplicationEvents.GetItemNotFound, LogLevel.Error },
     { ApplicationEvents.GetItemFound, LogLevel.Error },
+    { ApplicationEvents.UserAuthenticated, LogLevel.Information },
     { ApplicationEvents.UserNotAuthenticated, LogLevel.Error },
+    { ApplicationEvents.PasswordRestored, LogLevel.Information },
+    { ApplicationEvents.EmailRestored, LogLevel.Information },
     };
     }
 }
