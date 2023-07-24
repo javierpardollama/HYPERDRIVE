8326758d39b6ccf43755ea785c91c5fcdf1d805a
Javier Pardo <javierpardollama@outlook.com>
2023-07-24 23:00:55 +0200

    LIMPIEZA DE CODIGO

--- Hyperdrive.Client/src/app/management/grids/archive-shared-grid/archive-shared-grid.component.ts
+++ Hyperdrive.Client/src/app/management/grids/archive-shared-grid/archive-shared-grid.component.ts
@@ -2,13 +2,9 @@ import {
   AfterViewInit,
   Component,
   OnDestroy,
-  OnInit,
-  ViewChild
-} from '@angular/core';
+  OnInit} from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { ViewArchive } from './../../../../viewmodels/views/viewarchive';
