9ec8ec3791ac19b161709cc09564d22264afd134
Javier Pardo <javierpardollama@outlook.com>
2024-07-06 16:51:23 +0200

    PROTECCION XSS DINAMICA

--- Hyperdrive.Client/src/app/app.component.ts
+++ Hyperdrive.Client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private meta: Meta) {
+    this.ApplyContenSecurityPolicy();
+  }
+
+  ApplyContenSecurityPolicy(): void {
+    let content = `default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self'; connect-src ${environment.Api.Service}`;
+
+    this.meta.addTag({ 'http-equiv': 'Content-Security-Policy', content: content });
+  }
 }
