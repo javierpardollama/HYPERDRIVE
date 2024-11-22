3f3961d8c8bf2eef871182bd38a79951cf3cd91c
Javier Pardo <javierpardollama@outlook.com>
2024-11-22 11:49:34 +0100

    BORRADO DE ASPIRE HOST

--- Hyperdrive.Service/Hyperdrive.Tier.Host/Program.cs
+++
@@ -1,9 +0,0 @@
-using Aspire.Hosting;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-var service = builder.AddProject<Projects.Hyperdrive_Service>("service");
-
-var client = builder.AddNpmApp("client", "./../../Hyperdrive.Client");
-
-builder.Build().Run();
