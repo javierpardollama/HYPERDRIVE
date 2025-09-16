2dfff9674ff0524e9b6371dd975392b35dda1b90
Javier Pardo <javierpardollama@outlook.com>
2025-09-16 13:49:45 +0200

    ACCIONES CONTEXTUALES DE FICHEROS

--- Hyperdrive.Client/src/app/drive/modals/drive-item-context-menu/drive-item-context-menu.component.ts
+++ Hyperdrive.Client/src/app/drive/modals/drive-item-context-menu/drive-item-context-menu.component.ts
@@ -3,6 +3,9 @@ import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from "@angular/material/bott
 import { ViewDriveItem } from "../../../../viewmodels/views/viewdriveitem";
 import { DriveItemService } from "../../../../services/driveitem.service";
 import { BinaryService } from "../../../../services/binary.service";
+import { TextAppVariants } from "../../../../variants/text.app.variants";
+import { TimeAppVariants } from "../../../../variants/time.app.variants";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
     selector: 'app-drive-item-context-menu',
@@ -12,29 +15,39 @@ import { BinaryService } from "../../../../services/binary.service";
 export class DriveItemContextMenuComponent {
 
     // Constructor
-    constructor(public driveItemService: DriveItemService,
+    constructor(
+        public snackBar: MatSnackBar,
+        public driveItemService: DriveItemService,
         public binaryService: BinaryService,
-        public dialogRef: MatBottomSheetRef<DriveItemContextMenuComponent>,
+        public sheetRef: MatBottomSheetRef<DriveItemContextMenuComponent>,
         @Inject(MAT_BOTTOM_SHEET_DATA) public data: ViewDriveItem) { }
 
     public Share() {
-
+        this.sheetRef.dismiss();
     }
 
     public async Download() {
         const binary = await this.driveItemService.FindDriveItemBinaryById(this.data.Id);
         await this.binaryService.DecodeViewDriveItem(binary);
+        this.sheetRef.dismiss();
     }
 
     public Rename() {
-
+        this.sheetRef.dismiss();
     }
 
     public Activity() {
-
+        this.sheetRef.dismiss();
     }
 
-    public Remove() {
+    public async Remove(): Promise<void> {
+        await this.driveItemService.RemoveDriveItemById(this.data.Id);
+        
+        this.snackBar.open(
+            TextAppVariants.AppOperationSuccessCoreText,
+            TextAppVariants.AppOkButtonText,
+            { duration: TimeAppVariants.AppToastSecondTicks * TimeAppVariants.AppTimeSecondTicks });
 
+        this.sheetRef.dismiss();
     }
 }
