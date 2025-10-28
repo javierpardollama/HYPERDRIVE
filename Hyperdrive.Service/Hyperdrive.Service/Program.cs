e6985b32c3fc91a20fa6b314507f91d0df85d2ad
Javier Pardo <javierpardollama@outlook.com>
2025-10-28 08:56:34 +0100

    LIMPIEZA DE CODIGO

--- Hyperdrive.Service/Hyperdrive.Service/Program.cs
+++ Hyperdrive.Service/Hyperdrive.Service/Program.cs
@@ -1,11 +1,10 @@
-using System.Text.Json.Serialization;
 using Hyperdrive.Application.Installers;
 using Hyperdrive.Domain.Settings;
 using Hyperdrive.Infrastructure.Installers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using System.Text.Json.Serialization;
 
 var @builder = WebApplication.CreateBuilder(args);
 
