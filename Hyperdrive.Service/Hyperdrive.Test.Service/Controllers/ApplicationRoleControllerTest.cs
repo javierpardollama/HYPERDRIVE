3e0cd7f92bc5f8c4c8b7e9bbc6377e0a4b03fb22
Javier Pardo <javierpardollama@outlook.com>
2025-10-31 12:54:23 +0100

    TESTS

---
+++ Hyperdrive.Service/Hyperdrive.Test.Service/Controllers/ApplicationRoleControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using Hyperdrive.Application.ViewModels.Additions;
+using Hyperdrive.Application.ViewModels.Filters;
+using Hyperdrive.Application.ViewModels.Updates;
+using Hyperdrive.Application.ViewModels.Views;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Hyperdrive.Test.Service.Controllers
+{
+    public class ApplicationRoleControllerTest : BaseControllerTest
+    {
+        private static readonly HttpClient Client = new() { BaseAddress = new Uri("https://localhost:7297/api/v1/") };
+
+        protected ViewApplicationRole Role { get; set; }
+
+        [Test, Order(1)]
+        public async Task FindAllApplicationRole()
+        {
+            var response = await Client.GetAsync("all");
+            response.EnsureSuccessStatusCode();
+            var roles = await response.Content.ReadFromJsonAsync<List<ViewCatalog>>();
+
+            Assert.Pass();
+        }
+
+        [Test, Order(2)]
+        public async Task FindPaginatedApplicationRole()
+        {
+            var content = JsonContent.Create(new FilterPageApplicationRole { Index = 0, Size = 20, ApplicationUserId = User.Id });
+
+            var response = await Client.PostAsync("page", content);
+            response.EnsureSuccessStatusCode();
+            var page = await response.Content.ReadFromJsonAsync<ViewPage<ViewApplicationRole>>();
+
+            Assert.Pass();
+        }
+
+        [Test, Order(3)]
+        public async Task AddApplicationRole() 
+        {
+            var content = JsonContent.Create(new AddApplicationRole { ApplicationUserId = User.Id, Name = "Dungeon Master", ImageUri = "URL/Dungeon_Master_500px.png" });
+
+            var response = await Client.PostAsync("create", content);
+            response.EnsureSuccessStatusCode();
+            Role = await response.Content.ReadFromJsonAsync<ViewApplicationRole>();
+
+            Assert.Pass();
+        }
+
+        [Test, Order(4)]
+        public async Task UpdateApplicationRole() 
+        {
+            var content = JsonContent.Create(new UpdateApplicationRole { ApplicationUserId = User.Id, Id = Role.Id, Name = "Dungeon Master", ImageUri = "URL/Dungeon_Master_500px.png" });
+
+            var response = await Client.PutAsync("update", content);
+            response.EnsureSuccessStatusCode();
+            var role = await response.Content.ReadFromJsonAsync<ViewApplicationRole>();
+
+            Assert.Pass();
+        }
+
+        [Test, Order(5)]
+        public async Task RemoveApplicationRoleById()
+        {     
+            var response = await Client.DeleteAsync($"remove/{Role.Id}");
+            response.EnsureSuccessStatusCode();            
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ Hyperdrive.Service/Hyperdrive.Test.Service/Controllers/BaseControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using Hyperdrive.Application.ViewModels.Auth;
+using Hyperdrive.Application.ViewModels.Security;
+using Hyperdrive.Application.ViewModels.Views;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Hyperdrive.Test.Service.Controllers
+{
+    public class BaseControllerTest
+    {
+        private static readonly HttpClient Client = new() { BaseAddress = new Uri("https://localhost:7297/api/v1/") };
+
+        protected ViewApplicationUser User { get; set; }      
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            var content = JsonContent.Create(new AuthJoinIn { Email = "quorra@encom.com", Password = "P@ssw0rd" });
+
+            var response = await Client.PostAsync("auth/in", content);
+
+            response.EnsureSuccessStatusCode();
+
+            User = await response.Content.ReadFromJsonAsync<ViewApplicationUser>();
+
+            Assert.Pass();
+        }
+
+        [SetUp]
+        public void SetUp() 
+        {
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, User.Token.Value);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            var content = JsonContent.Create(new SecurityRefreshTokenReset { ApplicationUserId = User.Id, ApplicationUserRefreshToken = User.RefreshToken.Value });
+
+            var response = await Client.PutAsync("security/tokens/refresh", content);
+
+            response.EnsureSuccessStatusCode();
+
+            User = await response.Content.ReadFromJsonAsync<ViewApplicationUser>();
+
+            Assert.Pass();
+        }
+
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            var content = JsonContent.Create(new AuthSignOut { ApplicationUserId = User.Id, ApplicationUserRefreshToken = User.RefreshToken.Value });
+
+            var response = await Client.PostAsync("auth/out", content);
+
+            response.EnsureSuccessStatusCode();           
+
+            Assert.Pass();
+        }
+    }
+}
