da7197831ab90e20a584a62a3fe0a241d461464c
Javier Pardo <javierpardollama@outlook.com>
2025-11-03 15:50:03 +0100

    HELPER DE OBJETOS VACIOS

--- Hyperdrive.Client/src/helpers/value.helper.ts
+++ Hyperdrive.Client/src/helpers/value.helper.ts
@@ -11,12 +11,12 @@ export class ValueHelper {
 			return value.trim().length === 0;
 		}
 
-		if (typeof value === 'object') {
-			return Object.keys(value!).length === 0;
-		}
-
 		if (Array.isArray(value)) {
 			return value.length === 0;
+		}
+
+		if (typeof value === 'object') {
+			return Object.keys(value!).length === 0;
 		}
 
 		return false;
