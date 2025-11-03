05d49d370fcb1cd9f41d77407e13f0daa3114e25
Javier Pardo <javierpardollama@outlook.com>
2025-11-03 14:48:42 +0100

    HELPER DE OBJETOS VACIOS

--- Hyperdrive.Client/src/helpers/value.helper.ts
+++ Hyperdrive.Client/src/helpers/value.helper.ts
@@ -1,5 +1,24 @@
 export class ValueHelper {
-	public static IsNullOrEmpty(value: any): boolean {
-		return value === null || value === undefined || value === "" || value ==='';
+
+	public static IsDefined<T>(value: T): value is T {
+		return <T>value !== undefined && <T>value !== null;
+	}
+
+	public static IsEmpty<T>(value: T): boolean {
+		if (!this.IsDefined(value)) return true;
+
+		if (typeof value === 'string') {
+			return value.trim().length === 0;
+		}
+
+		if (typeof value === 'object') {
+			return Object.keys(value!).length === 0;
+		}
+
+		if (Array.isArray(value)) {
+			return value.length === 0;
+		}
+
+		return false;
 	}
 }
