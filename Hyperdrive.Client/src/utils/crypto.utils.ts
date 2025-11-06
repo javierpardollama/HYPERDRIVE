37ecd0114a2d063c4e36f4ad82cd8891d4f1043a
Javier Pardo <javierpardollama@outlook.com>
2025-11-06 09:09:45 +0100

    LIMPIEZA DE CODIGO

--- Hyperdrive.Client/src/utils/crypto.utils.ts
+++ Hyperdrive.Client/src/utils/crypto.utils.ts
@@ -1,6 +1,6 @@
-const algorithm = 'AES-CBC';
-const keyLength = 256; // bits
-const ivLength = 16;   // bytes
+const Algorithm = 'AES-CBC';
+const KeyLength = 256; // bits
+const IvLength = 16;   // bytes
 
 function Base64ToBytes(base64: string) {
     return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
@@ -8,31 +8,33 @@ function Base64ToBytes(base64: string) {
 
 export async function Encrypt(object: Record<string, any>): Promise<string> {
 
-    const iv = crypto.getRandomValues(new Uint8Array(ivLength));
-    const keyData = crypto.getRandomValues(new Uint8Array(keyLength / 8));
+    const iv = crypto.getRandomValues(new Uint8Array(IvLength));
+    const keydata = crypto.getRandomValues(new Uint8Array(KeyLength / 8));
 
     const key = await crypto.subtle.importKey(
         'raw',
-        keyData,
-        { name: algorithm },
+        keydata,
+        { name: Algorithm },
         false, // allow export
         ['encrypt']
     );
 
     const encoded = new TextEncoder().encode(JSON.stringify(object));
+    
     const encryptedBuffer = await crypto.subtle.encrypt(
-        { name: algorithm, iv },
+        { name: Algorithm, iv },
         key,
         encoded
     );
 
-    const exportedKey = await crypto.subtle.exportKey('raw', key);
-    const keyBase64 = btoa(String.fromCharCode(...new Uint8Array(exportedKey)));
+    const exportedkey = await crypto.subtle.exportKey('raw', key);
+
+    const keybase64 = btoa(String.fromCharCode(...new Uint8Array(exportedkey)));
 
     const encrypted = btoa(String.fromCharCode(...new Uint8Array(encryptedBuffer)));
 
     const data = {
-        Key: keyBase64,
+        Key: keybase64,
         Iv: btoa(String.fromCharCode(...iv)),
         Data: encrypted
     };
@@ -45,27 +47,27 @@ export async function Decrypt(jsonstring: string): Promise<any> {
 
     const data = JSON.parse(jsonstring);
 
-    const keyBytes = Base64ToBytes(data.Key);
+    const keybytes = Base64ToBytes(data.Key);
     const iv = Base64ToBytes(data.Iv);
-    const encryptedBytes = Base64ToBytes(data.Data);
+    const encryptedbytes = Base64ToBytes(data.Data);
 
     // Import the key
     const key = await crypto.subtle.importKey(
         'raw',
-        keyBytes,
-        { name: algorithm },
+        keybytes,
+        { name: Algorithm },
         false,
         ['decrypt']
     );
 
     // Decrypt
-    const decryptedBuffer = await crypto.subtle.decrypt(
-        { name: algorithm, iv },
+    const decryptedbuffer = await crypto.subtle.decrypt(
+        { name: Algorithm, iv },
         key,
-        encryptedBytes
+        encryptedbytes
     );
 
-    const decryptedText = new TextDecoder().decode(decryptedBuffer);
-    return JSON.parse(decryptedText);
+    const decryptedtext = new TextDecoder().decode(decryptedbuffer);
+    return JSON.parse(decryptedtext);
 }
 
