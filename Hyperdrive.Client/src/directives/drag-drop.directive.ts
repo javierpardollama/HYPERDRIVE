184016b7d9f70769dca192e19e0c8ac431eb259b
Javier Pardo <javierpardollama@outlook.com>
2025-12-16 10:20:38 +0100

    LIMPIEZA DE CODIGO

--- Hyperdrive.Client/src/directives/drag-drop.directive.ts
+++ Hyperdrive.Client/src/directives/drag-drop.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+import { Directive, HostListener, Output, EventEmitter, HostBinding } from '@angular/core';
 
 @Directive({
     selector: '[app-drag-drop]'
