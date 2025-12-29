8642775e1b3219cabd126a62ca1c17fdf15e7931
Javier Pardo <javierpardollama@outlook.com>
2025-12-29 09:09:30 +0100

    CORRECCION CAMBIO DE MODELO DE DATOS

--- Hyperdrive.Client/src/utils/blob.utils.ts
+++ Hyperdrive.Client/src/utils/blob.utils.ts
@@ -1,12 +1,10 @@
-import { DecodeBase64 } from "./byte.utils";
+import { DecodeBase64, EncodeBase64 } from "./byte.utils";
 
 export async function EncodeBlob(file: File): Promise<string> {
 
     const bytes = new Uint8Array(await file.arrayBuffer());
 
-    const binarystring = Array.from(bytes).map(byte => String.fromCharCode(byte)).join('');
-
-    return window.btoa(binarystring);
+    return EncodeBase64(bytes);
 }
 
 export async function DecodeBlob(content: string, type: string): Promise<Blob> {
