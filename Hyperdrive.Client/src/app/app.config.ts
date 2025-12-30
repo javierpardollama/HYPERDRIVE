decc54abd76b213916754456b504737532990f73
Javier Pardo <javierpardollama@outlook.com>
2025-12-30 15:47:52 +0100

    MIGRACION INTERCEPTOR ANGULAR 21

--- Hyperdrive.Client/src/app/app.config.ts
+++ Hyperdrive.Client/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { AuthInterceptor } from '../interceptors/auth.interceptor';
@@ -9,7 +9,6 @@ export const AppConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptors([AuthInterceptor]))
     ]
 };
--- Hyperdrive.Client/src/interceptors/auth.interceptor.ts
+++ Hyperdrive.Client/src/interceptors/auth.interceptor.ts
@@ -1,46 +1,32 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 
-import { from, Observable, switchMap } from 'rxjs';
+import { from, switchMap } from 'rxjs';
 
 import { ViewApplicationUser } from '../viewmodels/views/viewapplicationuser';
 import { IsEmpty } from 'src/utils/object.utils';
 import { SecureStorageService } from 'src/services/secure.storage.service';
 import { VaultKeyAppVariants } from 'src/variants/vault.keys.variants';
 
-@Injectable({
-  providedIn: 'root'
-})
 
-export class AuthInterceptor implements HttpInterceptor {
-  // DI
-  private secureStorageService = inject(SecureStorageService);
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+  const secureStorageService = inject(SecureStorageService);
 
-  private User?: ViewApplicationUser;
-
-  constructor() { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.GetLocalUser()).pipe(
-      switchMap(() => {
-        if (!IsEmpty(this.User)) {
-          req = req.clone({
-            setHeaders: {
-              'Content-Type': 'application/json; charset=utf-8',
-              'Accept': 'application/json',
-              'Authorization': `Bearer ${this.User?.Token?.Value}`,
-            },
-          });
-        }
-        return next.handle(req); // return original Observable
-      })
-    );
-  }
-
-
-  // Get User from Storage
-  public async GetLocalUser(): Promise<void> {
-    this.User = await this.secureStorageService.RetrieveObject<ViewApplicationUser>(VaultKeyAppVariants.VAULT_USER_KEY);
-  }
-}
+  return from(
+    secureStorageService.RetrieveObject<ViewApplicationUser>(VaultKeyAppVariants.VAULT_USER_KEY)
+  ).pipe(
+    switchMap((user) => {
+      if (!IsEmpty(user)) {
+        req = req.clone({
+          setHeaders: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${user?.Token?.Value}`,
+          },
+        });
+      }
+      return next(req); // return original Observable
+    })
+  );
+};
