00655a87c889c133d2ad23fa7b994feda7a21de7
Javier Pardo <javierpardollama@outlook.com>
2026-02-12 08:22:04 +0100

    CODIGO DE ERROR HTTP

--- Hyperdrive.Client/src/variants/codes.app.variants.ts
+++ Hyperdrive.Client/src/variants/codes.app.variants.ts
@@ -5,8 +5,10 @@ export class CodeAppVariants {
     public static readonly BAD_REQUEST = 400;
     public static readonly UNAUTHORIZED = 401;
     public static readonly NOT_FOUND = 404;
+    public static readonly REQUEST_TIMEOUT = 408;
     public static readonly CONFLICT = 409;
     public static readonly UMPROCESSABLE_CONTENT = 422;
+    public static readonly FAILED_DEPENDENCY = 424;
     public static readonly INTERNAL_SERVER_ERROR = 500;
     public static readonly SERVICE_UNAVAILABLE = 503;
 }
